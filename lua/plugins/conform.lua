05c79116789f052446adf838163efd9aad9168e1
Brian McDaniel <brianmcd05@gmail.com>
2024-09-02 09:56:17 -0400

    Use conform to format graphql

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require('conform').setup({
       formatters_by_ft = {
+        graphql = { { "prettier" } },
         html = { { "prettier" } },
         javascript = { { "prettier" } },
         javascriptreact = { { "prettier" } },
