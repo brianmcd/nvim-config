12e2fd38426f6ec522ef0ea83fb6496b4c6f8ba9
Brian McDaniel <brianmcd05@gmail.com>
2024-09-02 14:57:58 -0400

    Fix tab

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -13,7 +13,13 @@ return {
   config = function ()
     local cmp = require("cmp")
 
-    -- TODO: see which of these keymappings
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    -- TODO: see which of these keymappings I actually need
     cmp.setup{
       snippet = {
         expand = function(args)
