e8f73800553fa4662a9399a1b0eb2f108e02e8fd
Brian McDaniel <brianmcd05@gmail.com>
2024-09-02 16:26:35 -0400

    Better mason config

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -1,6 +1,30 @@
 return {
   "williamboman/mason.nvim",
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+  },
   config = function()
     require("mason").setup()
+
+    require("mason-lspconfig").setup {
+      ensure_installed = { "eslint" },
+      automatic_installation = true,
+    }
+
+    require("mason-lspconfig").setup_handlers {
+      function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+      end,
+      ["eslint"] = function ()
+        require("lspconfig").eslint.setup {
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+        }
+      end
+    }
   end
 }
