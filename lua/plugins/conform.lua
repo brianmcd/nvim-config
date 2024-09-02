378eed1c59c476168bbf74c65935974d3cf64ec8
Brian McDaniel <brianmcd05@gmail.com>
2024-09-02 15:41:03 -0400

    Get eslint going

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,14 +3,14 @@ return {
   config = function()
     require('conform').setup({
       formatters_by_ft = {
-        graphql = { { "prettier" } },
-        html = { { "prettier" } },
-        javascript = { { "prettier" } },
-        javascriptreact = { { "prettier" } },
-        json = { { "prettier" } },
-        typescript = { { "prettier" } },
-        typescriptreact = { { "prettier" } },
-        vue = { { "prettier" } },
+        graphql = { "prettier" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        json = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
         ["_"] = { "trim_whitespace" },
       },
       format_on_save = {
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,5 +5,14 @@ return {
     vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<Leader>e', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+
+    require'lspconfig'.eslint.setup{
+      on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          command = "EslintFixAll",
+        })
+      end,
+    }
   end
 }
