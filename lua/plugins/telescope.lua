57a7f5ef1ae9e3a6a75387e5af54f64cef70aa08
Brian McDaniel <brianmcd05@gmail.com>
2024-09-03 15:25:13 -0400

    Add native sorter for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    }
+  },
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
