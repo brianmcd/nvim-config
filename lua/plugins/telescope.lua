ab632a9c9ddea4911a1d3d24eee00163564f9c2f
Brian McDaniel <brianmcd05@gmail.com>
2024-09-11 08:11:29 -0400

    Use vertical layout strategy for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,9 +9,15 @@ return {
     }
   },
   config = function()
+    require('telescope').setup({
+      defaults = {
+        layout_strategy = 'vertical',
+      },
+    })
+
     local builtin = require('telescope.builtin')
+
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    -- vim.keymap.set('n', '<C-p>', builtin.find_files(require('telescope.themes').get_dropdown({})), {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
