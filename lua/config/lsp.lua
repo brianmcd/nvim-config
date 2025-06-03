b3268b7ba1c450a9b576da2067179f99a276d2d3
Brian McDaniel <brianmcd05@gmail.com>
2025-06-03 10:20:25 -0400

    Disable semantic tokens

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,11 +5,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local bufnr = ev.buf
     local server_name = client.name
 
-    if server_name == "eslint" then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        command = "EslintFixAll",
-      })
+    -- There's a regression somewhere that causes this to fail.
+    --if server_name == "eslint" then
+    --vim.api.nvim_create_autocmd("BufWritePre", {
+      --buffer = bufnr,
+      --command = "eslint.applyAllFixes",
+      --})
+      --end
     end
-  end
+  })
+
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+  desc = "Disable semantic tokens for typescript",
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == "typescript" then
+      local client = vim.lsp.get_client_by_id(opts.data.client_id)
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
 })
+
